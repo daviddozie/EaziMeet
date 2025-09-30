d8ba2c1733460de20cde1fda4d474515270105a6
daviddozie <davidmgbede75@gmail.com>
2025-09-30 23:40:48 +0100

    resolved bug

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,20 +1,19 @@
 import z from "zod";
 
 export const CreateMeetingSchema = z.object({
-    meetingTitle: z
-        .string()
-        .trim()
-        .min(2, { message: "Please enter the meeting title" })
-        .max(100, { message: "Meeting title should not exceed 100 characters" }),
+  meetingTitle: z
+    .string()
+    .trim()
+    .min(2, { message: "Please enter the meeting title" })
+    .max(100, { message: "Meeting title should not exceed 100 characters" }),
 
-    password: z.string().trim().optional(),
+  password: z.string().trim().optional(),
 
-    date: z.string().min(1, { message: "Please select a date" }),
+  date: z.string().min(1, { message: "Please select a date" }),
+  time: z.string().min(1, { message: "Please select a time" }),
 
-    time: z.string().min(1, { message: "Please select a time" }),
-
-    enableChat: z.boolean().optional().default(true),
-    recordMeeting: z.boolean().optional().default(false),
+  enableChat: z.boolean(),
+  recordMeeting: z.boolean(),
 });
 
 export type CreateMeetingSchemaType = z.infer<typeof CreateMeetingSchema>;
